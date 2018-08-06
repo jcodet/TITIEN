7eceda72204a31a7cf0051cb466328a9055fc8af
Ju <julien.codet@gmail.com>
2018-08-06 18:26:55 +0200

    Ajout d'un test

---
+++ src/test/java/com/rte/kafka/ProducerTest.java
@@ -0,0 +1,15 @@
+package java.com.rte.kafka;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProducerTest {
+
+	@Test
+	public void shouldBeOK() {
+		assertTrue(true);
+		
+	}
+
+}
