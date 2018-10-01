740051e52c674d9f83fc21d27f4bf09418dab526
Ju <julien.codet@gmail.com>
2018-10-01 15:40:40 +0200

    Add répertoire pipeline utils

---
+++ runSlave.sh
@@ -0,0 +1,1 @@
+java -jar agent.jar -jnlpUrl http://localhost:8080/computer/titien/slave-agent.jnlp -secret c376c1b6cd5ad8b1dc3141ffa9df410c6415982d8a0d9ba91c7e011db443b17d -workDir "/var/lib/jenkins"
