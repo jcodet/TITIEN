017c72fa32205f8da7bb243786198c4e17bad845
Ju <julien.codet@gmail.com>
2018-10-01 16:05:51 +0200

    Fix POM

--- pipeline/utils/myScript.groovy
+++ pipeline/utils/myScript.groovy
@@ -3,7 +3,7 @@
  * @param pomPath la path vers le pom
  * @return la version
  */
-def getReleaseVersion(pomPath = './') {
+def getReleaseVersion(pomPath = 'application/') {
     def matcher = readFile(pomPath + 'pom.xml') =~ '<version>(.+)-SNAPSHOT</version>'
     releaseVersion = matcher[0][1]
     return releaseVersion
